f089057a4cef99a6f604fec7aa4cc1bdc984217d
Maxim Bondarenko <maximbndrnk@gmail.com>
2023-03-18 11:48:51 +0200

    add cors, morgan packages

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
+const morgan = require('morgan');
 
+// ROUTES
 const authRoutes = require('./routes/auth');
 const expensesRoutes = require('./routes/expenses');
 
 const app = express();
 
+app.use(morgan('dev'));
+app.use(cors());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
