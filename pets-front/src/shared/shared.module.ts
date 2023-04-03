ec4eb4fd6bb6c5d2db8f4958751c399825838cad
Maxim Bondarenko <maximbndrnk@gmail.com>
2023-04-03 21:21:22 +0300

    add header

--- pets-front/src/shared/shared.module.ts
+++ pets-front/src/shared/shared.module.ts
@@ -1,12 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './components/header/header.component';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatButtonModule } from '@angular/material/button';
 
 
 
 @NgModule({
-  declarations: [],
+    declarations: [
+        HeaderComponent
+    ],
+    exports: [
+        HeaderComponent
+    ],
   imports: [
-    CommonModule
+    CommonModule,
+    MatIconModule,
+    MatMenuModule,
+    MatButtonModule
   ]
 })
 export class SharedModule { }
