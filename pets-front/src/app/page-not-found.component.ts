649cda2df845b980dcb130faf89fa899128a2fa9
Maxim Bondarenko <maximbndrnk@gmail.com>
2023-04-03 11:31:03 +0300

    add login and shared module

---
+++ pets-front/src/app/page-not-found.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-page-not-found',
+  template: `
+  <div> Page not found </div>
+  `,
+  styleUrls: []
+})
+export class PageNotFoundComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
